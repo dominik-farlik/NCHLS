4af8332d886f5e4762836d3638137393975b714d
Dominik Farlík <dominik@farlik.cz>
2026-01-26 16:42:32 +0100

    del unused domain

--- React/vite.config.js
+++ React/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -10,6 +9,5 @@ export default defineConfig({
         target: 'http://fastapi:8000',
       }
     },
-    allowedHosts: ['victus.home'],
   }
 })
