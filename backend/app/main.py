f7a7925f9cd780c910a1e0eefc5035f4def275f9
Dominik Farlík <dominik@farlik.cz>
2026-09-10 21:53:56 +0200

    adjust allowed origins and upload dir for sds

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,28 +3,29 @@ from pathlib import Path
 from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.core.auth import get_current_subject
-from app.core.config import settings
-from app.routers import substances, dictionaries, health, records, export_docs, auth
+from app.routers import substances, dictionaries, health, auth, records
+from config import get_settings
 
 app = FastAPI(title="NCHLS API", version="1.0.0")
 
+origins = [
+    get_settings().FRONTEND_URL,
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.ALLOW_ORIGINS,
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-UPLOAD_DIR = Path(settings.UPLOAD_DIR)
+UPLOAD_DIR = Path(get_settings().UPLOAD_DIR)
 UPLOAD_DIR.mkdir(parents=True, exist_ok=True)
 
-#AuthDep = Depends(get_current_subject)
-
 app.include_router(health.router, prefix="/api", tags=["health"])
-app.include_router(dictionaries.router, prefix="/api", tags=["dictionaries"], dependencies=[])
-app.include_router(substances.router, prefix="/api/substances", tags=["substances"], dependencies=[])
-#app.include_router(records.router, prefix="/api/records", tags=["records"], dependencies=[])
+app.include_router(dictionaries.router, prefix="/api", tags=["dictionaries"])
+app.include_router(substances.router, prefix="/api/substances", tags=["substances"])
+app.include_router(records.router, prefix="/api/records", tags=["records"])
 app.include_router(auth.router, prefix="/api/auth", tags=["auth"])
 #app.include_router(export_docs.router, prefix="/api/export", tags=["export"], dependencies=[])
