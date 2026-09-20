7bfcb115716f4c2b664a829b77a9702eca86bd81
Dominik Farlík <dominik@farlik.cz>
2026-09-20 23:45:17 +0200

    delete old App.css config + update App.tsx syntax

---
+++ frontend/src/App.tsx
@@ -0,0 +1,42 @@
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import AddRecord from "./pages/AddRecord.jsx";
+import AddSubstance from "./pages/AddSubstance.jsx";
+import Home from "./pages/Home.jsx";
+import Substances from "./pages/Substances.tsx";
+import Records from "./pages/Records.jsx";
+import EditSubstance from "./pages/EditSubstance.jsx";
+import Departments from "./pages/Departments.jsx";
+import Inventory from "./pages/Inventory.jsx";
+import EditRecord from "./pages/EditRecord.jsx";
+import RequireAuth from "./components/RequireAuth.jsx";
+import Login from "./pages/Login.jsx";
+
+
+const router = createBrowserRouter(
+    [
+        {path: "/", element: <Home/>},
+        {path: "/login", element: <Login/>},
+        {element: <RequireAuth/>, children: [
+                {path: "/substances", element: <Substances/>},
+                {path: "/add-substance", element: <AddSubstance/>},
+                {path: "/edit-substance/:substanceId", element: <EditSubstance/>},
+                {path: "/records", element: <Records/>},
+                {path: "/records/:departmentName", element: <Records/>},
+                {path: "/departments", element: <Departments/>},
+                {path: "/inventory/:departmentName", element: <Inventory/>},
+                {path: "/add-record", element: <AddRecord/>},
+                {path: "/edit-record/:recordId", element: <EditRecord/>},
+
+        ]},
+    ]
+);
+
+function App() {
+    return (
+        <>
+            <RouterProvider router={router}/>
+        </>
+    );
+}
+
+export default App;
